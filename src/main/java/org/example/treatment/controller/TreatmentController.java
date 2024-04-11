6bca2762b406af4116f8c73e21ff939417091a15
BackJiwan <bn1357nb@gmail.com>
2024-04-11 17:38:45 +0900

    fix: TreatmentController 생성자 수정

--- src/main/java/org/example/treatment/controller/TreatmentController.java
+++ src/main/java/org/example/treatment/controller/TreatmentController.java
@@ -1,5 +1,7 @@
 package org.example.treatment.controller;
 import lombok.RequiredArgsConstructor;
+import org.example.animal.repository.AnimalRepository;
+import org.example.animal.service.AnimalService;
 import org.example.member.repository.MemberRepository;
 import org.example.member.service.MemberService;
 import org.example.member.view.MemberView;
@@ -10,10 +12,15 @@ import org.example.treatment.view.TreatmentView;
 @RequiredArgsConstructor
 public class TreatmentController {
     private final TreatmentRepository treatmentRepository = new TreatmentRepository();
+    private final MemberRepository memberRepository = new MemberRepository();
+    private final AnimalRepository animalRepository = new AnimalRepository();
     private TreatmentService treatmentService = new TreatmentService(treatmentRepository);
+    private MemberService memberService = new MemberService(memberRepository);
+    private AnimalService animalService = new AnimalService(animalRepository);
 
     public void treatmentMenu(){
-        TreatmentView view = new TreatmentView(treatmentService,treatmentRepository);
+        TreatmentView view = new TreatmentView(treatmentService,treatmentRepository,memberService,
+                memberRepository,animalService,animalRepository);
         view.treatmentView();
     }
 }
